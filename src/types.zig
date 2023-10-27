0d04b94ea903781ec5db13b2cdc3c182e916e5e1
André Willyan <andrewillyan@tutanota.com>
2023-10-27 14:20:58 -0300

    add exaustive test to types.zig

--- src/types.zig
+++ src/types.zig
@@ -43,28 +43,56 @@ pub const FnType = struct {
 };
 
 test "types test" {
-    const in: Type = Type.nat;
-    const out: Type = Type.boolean;
-    const t = FnType.init(&in, &out).intoType();
+    const n: Type = Type.nat;
+    const b: Type = Type.boolean;
+    const f1 = FnType.init(&n, &b).intoType();
+    const f2 = FnType.init(&n, &n).intoType();
+    const f3 = FnType.init(&b, &b).intoType();
+    const f4 = FnType.init(&f2, &f2).intoType();
     // verify types
-    try std.testing.expect(in == .nat);
-    try std.testing.expect(out == .boolean);
-    try std.testing.expect(t == .fun);
+    try std.testing.expect(n == .nat);
+    try std.testing.expect(b == .boolean);
+    try std.testing.expect(f1 == .fun);
+    try std.testing.expect(f2 == .fun);
+    try std.testing.expect(f3 == .fun);
+    try std.testing.expect(f4 == .fun);
 }
 
 test "type equality test" {
     const n: Type = Type.nat;
     const b: Type = Type.boolean;
-    const f1 = FnType.init(&n, &b);
-    const f2 = FnType.init(&n, &n);
+    const f1 = FnType.init(&n, &b).intoType();
+    const f2 = FnType.init(&n, &n).intoType();
+    const f3 = FnType.init(&b, &b).intoType();
     // equal
     try expect(b.eq(&b));
     try expect(n.eq(&n));
     try expect(f1.eq(&f1));
     try expect(f2.eq(&f2));
+    try expect(f3.eq(&f3));
     // not equal
     try expect(!b.eq(&n));
+    try expect(!b.eq(&f1));
+    try expect(!b.eq(&f2));
+    try expect(!b.eq(&f3));
+
     try expect(!n.eq(&b));
+    try expect(!n.eq(&f1));
+    try expect(!n.eq(&f2));
+    try expect(!n.eq(&f3));
+
+    try expect(!f1.eq(&b));
+    try expect(!f1.eq(&n));
     try expect(!f1.eq(&f2));
+    try expect(!f1.eq(&f3));
+
+    try expect(!f2.eq(&b));
+    try expect(!f2.eq(&n));
     try expect(!f2.eq(&f1));
+    try expect(!f2.eq(&f3));
+
+    try expect(!f3.eq(&b));
+    try expect(!f3.eq(&n));
+    try expect(!f3.eq(&f1));
+    try expect(!f3.eq(&f2));
 }
